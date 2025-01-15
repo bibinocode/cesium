5dd9aeb082faddf84cf3ddb788fd16694b3db1d7
阿逼 <2553400824@qq.com>
2025-01-15 21:24:25 +0800

    feat: 设置token 设置界面控件 设置默认视图位置

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
+import * as Cesium from 'cesium'
+import '../public/Cesium/Widgets/widgets.css'
+
+
+window.CESIUM_BASE_URL = '/public/Cesium/'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    // 设置自己的token
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmZmFmOWMzNS01NGM5LTQyZWItOGVkZS02NTQ1NzgwNzJhZDMiLCJpZCI6MTE1NzM1LCJpYXQiOjE3MzY5NDcxNjh9.1qDttoLbk0UUEgZQCNDPK6vf6D0f1tGE12Lxior1c88'
+    // 设置默认视角 
+    Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
+      // 西经,
+      89.5,
+      // 南纬
+      20.4,
+      // 东经
+      110.4,
+      // 北纬 
+      61.2
+    )
+    const viewer = new Cesium.Viewer('cesiumContainer', {  
+      // 搜索框控件隐藏
+      geocoder: false,
+      // 回到默认视角
+      homeButton: false,
+      // 2D/3D视角切换
+      sceneModePicker: false,
+      // 图层切换
+      baseLayerPicker: false,
+      // 帮助按钮
+      navigationHelpButton: false,
+      // 自转动画
+      animation: false,
+      // 时间轴
+      timeline: false,
+      // 全屏按钮
+      fullscreenButton:false
+    })
+    // 隐藏版权信息
+    viewer.cesiumWidget.creditContainer.style.display = 'none'
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id='cesiumContainer'></div>
     </>
   )
 }
